07dc3d2790de1647181ae700d8494fb1f53d6aec
Marl0023 <marlotay2312@gmail.com>
2024-10-17 12:18:15 -0500

    Unico commit

---
+++ Funciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problema1
+{
+    public class Funciones
+    {
+        int p =0, n =0;
+        public int negativos() { 
+            return n++;
+        }
+        public int positivos() {
+            return p++;
+        }
+        public void reset()
+        {
+            n=0;
+            p=0;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using problema1;
+using System.Diagnostics.CodeAnalysis;
+int valor;
+char opcion;
+Random rnd = new Random();
+Funciones fun = new Funciones();
+do
+{
+
+    do
+    {
+        Console.Write("Se va a generar aleatoriamente N numero de valores, ingrese cuantos desea: ");
+    } while (!int.TryParse(Console.ReadLine(), out valor) || valor <= 0);
+    Console.WriteLine("\nLista aleatoria");
+    for (int i = 0; i < valor; i++)
+    {
+        int aleatorio = rnd.Next(-20, 21);
+        Console.WriteLine("\n" + aleatorio);
+        if (aleatorio > 0)
+        {
+            fun.positivos();
+        }
+        else if (aleatorio < 0)
+        {
+            fun.negativos();
+        }
+    }
+    Console.WriteLine("\n #Numeros Negativos:  " + fun.negativos());
+    Console.WriteLine("#Numeros Positivos: " + fun.positivos());
+    do
+    {
+        Console.Write("\n Desea volver a intentar? (s/n) : ");
+        opcion = char.ToLower(Console.ReadKey().KeyChar);
+    } while (opcion != 's' && opcion != 'n');
+    fun.reset();
+    Console.Clear();
+} while (opcion == 's');
+Console.WriteLine("Graciela :v ");
